929e041557ce6a8567ba15f9cc3b21ab261311eb
Kayc Prado <kaycps_15@hotmail.com>
2025-05-17 10:30:41 -0300

    Remove propriedade Nome da entidade Livro

---
+++ Infrastructure/Migrations/20250517132738_AjusteEntidadeLivro.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesafioCCAA.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteEntidadeLivro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                table: "Livros");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                table: "Livros",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
